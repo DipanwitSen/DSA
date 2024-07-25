fc9a4a25927f27d3d48a23d49b986cabbc601fac
DipanwitSen <130593147+DipanwitSen@users.noreply.github.com>
2024-07-25 10:10:14 +0530

    Create prog3.c

---
+++ Day_1/prog3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//Name-Dipanwita Sen
+//Roll=22052204
+int main() {
+    int a[100], b[100], n;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    
+    b[0] = a[0];
+    printf("%d ", b[0]);  // Print the first element of b
+    
+    for (int i = 1; i < n; i++) {
+        b[i] = b[i-1] + a[i];
+        printf("%d ", b[i]);
+    }
+    
+    printf("\n");  // Add a newline at the end for better formatting
+    
+    return 0;
+}
