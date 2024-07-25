fc6a081f3a8ec5f3e13c99448e7f04f0ffd497c9
DipanwitSen <130593147+DipanwitSen@users.noreply.github.com>
2024-07-25 10:11:41 +0530

    Create prog5.c

---
+++ Day_1/prog5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+//Name-Dipanwita Sen
+//Roll=22052204
+void EXCHANGE(int *A, int p, int q) {
+    int temp = A[p];
+    A[p] = A[q];
+    A[q] = temp;
+}
+
+void ROTATE_RIGHT(int *A, int b) {
+    if (b < 1) return;  
+    int temp = A[b - 1];
+    for (int i = b - 1; i > 0; i--) {
+        A[i] = A[i - 1];
+    }
+    A[0] = temp;
+}
+
+int main() {
+    int A[100], n, b;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter elements of array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+    printf("Enter rotation place: ");
+    scanf("%d", &b);
+    
+    printf("\nBefore Rotation:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+    ROTATE_RIGHT(A, b);
+    
+    printf("\nAfter Rotation:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
