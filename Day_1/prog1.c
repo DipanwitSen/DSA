d4b04606c3e6836469a1e6108c1b3073a7071ecc
DipanwitSen <130593147+DipanwitSen@users.noreply.github.com>
2024-07-25 10:07:47 +0530

    Create prog1.c

---
+++ Day_1/prog1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() {
+    printf("Name: Dipanwita Sen\n");
+    printf("Roll: 22052204\n");
+
+    FILE *inputFile;
+
+
+    inputFile = fopen("input.txt", "r+");
+    if (inputFile == NULL) {
+        printf("Invalid input file\n");
+        return 1;
+    }
+    int num;
+    for (int i = 0; i < 10; i++) {
+        if (fscanf(inputFile, "%d", &num) == 1) {
+            printf("%d\n", num); // Print to terminal
+            fprintf(inputFile, "%d\n", num); // Print to file
+        } else {
+            printf("Error reading integer from input file\n");
+            break;
+        }
+    }
+
+    fclose(inputFile);
+
+    return 0;
+}
