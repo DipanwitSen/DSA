bc586abd8b836e35ba738b5874a5109fe05a38e6
DipanwitSen <130593147+DipanwitSen@users.noreply.github.com>
2024-07-31 22:17:43 +0530

    Create prog3.c

---
+++ Day_2/prog3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void binary_Search(int target, int low, int high, int a[], int *c) {
+    if (low > high) {
+        printf("Target not found.\n");
+        return;
+    }
+
+    int mid = (low + high) / 2;
+    (*c)++;
+
+    if (target == a[mid]) {
+        printf("Found at index %d with %d comparisons.\n", mid, *c);
+        return;
+    }
+    else if (target < a[mid]) {
+        binary_Search(target, low, mid - 1, a, c);
+    }
+    else {
+        binary_Search(target, mid + 1, high, a, c);
+    }
+}
+
+int main() {
+    int n, target;
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter array elements in sorted order: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    int comparisons = 0;
+    binary_Search(target, 0, n - 1, arr, &comparisons);
+
+    return 0;
+}
