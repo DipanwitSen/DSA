c31a6141a4d9b819ffa3246eab6315f23e1f2003
DipanwitSen <130593147+DipanwitSen@users.noreply.github.com>
2024-08-08 10:41:16 +0530

    Update prog3.c

--- Day_3/prog3.c
+++ Day_3/prog3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -8,24 +9,18 @@ void swap(int *a, int *b) {
 }
 
 int partition(int a[], int lb, int ub) {
-    int pivot = a[lb];
-    int start = lb;
-    int end = ub;
-
-    while (start < end) {
-        while (a[start] <= pivot && start < end) {
-            start++;
-        }
-        while (a[end] > pivot) {
-            end--;
-        }
-        if (start < end) {
-            swap(&a[start], &a[end]);
+    int pivot = a[ub];
+    int i = lb - 1;     
+    for (int j = lb; j <= ub - 1; j++) {
+        if (a[j] < pivot) {
+            i++; 
+            swap(&a[i], &a[j]);
         }
     }
-    swap(&a[lb], &a[end]);
-    return end;
+    swap(&a[i + 1], &a[ub]);
+    return (i + 1);
 }
+
 void quick_sort(int a[], int p, int r) {
     if (p < r) {
         int loc = partition(a, p, r);
@@ -39,21 +34,23 @@ int main() {
     int *a, n;
     printf("Enter size of array: ");
     scanf("%d", &n);
-    a = (int*)malloc(n * sizeof(int)); 
+    a = (int*)malloc(n * sizeof(int));
     for(int i = 0; i < n; i++) {
         printf("Enter element of array index %d: ", i);
         scanf("%d", &a[i]);
     }
     int r = rand() % n;
     printf("Random number from array: %d\n", a[r]);
-    swap(&a[r], &a[n-1]);
-    printf("\n");
-    int pivot=n-1;
-    quick_sort(a,0,pivot);
+    swap(&a[r], &a[n-1]); 
+
+    quick_sort(a, 0, n - 1);
+
+    printf("Sorted array: ");
     for(int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
     printf("\n");
+
     free(a);
     return 0;
 }
